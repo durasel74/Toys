5efd76a05ccae5e1ad1129ec1dcee0487882cc38
durasel74 <diman174cheb@mail.ru>
2021-12-14 00:16:04 +0500

    Client table output complete

--- ToysClient/ToysClient/VM/ViewModel.cs
+++ ToysClient/ToysClient/VM/ViewModel.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Collections.ObjectModel;
@@ -6,15 +9,100 @@ using ToysClient.Model;
 
 namespace ToysClient.VM
 {
-	public class ViewModel
+	public class ViewModel : INotifyPropertyChanged
 	{
+		private ClientLan client;
 		public ViewModel()
 		{
-			var client = new ClientLan("127.0.0.1", 8888);
-			var result = client.SendRequest("getjournals");
-			Console.WriteLine(result);
+			client = new ClientLan("127.0.0.1", 8888);
 		}
 
+		public ObservableCollection<Client> Clients { get; set; }
+		public ObservableCollection<Seller> Sellers { get; set; }
+		public ObservableCollection<Sklad> Sklads { get; set; }
+		public ObservableCollection<Toy> Toys { get; set; }
+		public ObservableCollection<Journal> Journals { get; set; }
+
+		private ButtonCommand clientGetCommand;
+		public ButtonCommand ClientGetCommand
+		{
+			get
+			{
+				return clientGetCommand ??
+				  (clientGetCommand = new ButtonCommand(obj =>
+				  {
+					  var resultRequest = client.SendRequest("getclients");
+					  var result = JsonSerializer.Deserialize<List<Client>>(resultRequest);
+					  Clients = new ObservableCollection<Client>(result);
+					  OnPropertyChanged("Clients");
+					  Console.WriteLine(Clients[0].IdClient);
+				  }));
+			}
+		}
+
+		private ButtonCommand sellerGetCommand;
+		public ButtonCommand SellerGetCommand
+		{
+			get
+			{
+				return sellerGetCommand ??
+				  (sellerGetCommand = new ButtonCommand(obj =>
+				  {
+					  var resultRequest = client.SendRequest("getsellers");
+					  var result = JsonSerializer.Deserialize<List<Seller>>(resultRequest);
+					  Sellers = new ObservableCollection<Seller>(result);
+					  OnPropertyChanged("Sellers");
+				  }));
+			}
+		}
+
+		private ButtonCommand skladGetCommand;
+		public ButtonCommand SkladGetCommand
+		{
+			get
+			{
+				return skladGetCommand ??
+				  (skladGetCommand = new ButtonCommand(obj =>
+				  {
+					  var resultRequest = client.SendRequest("getsklads");
+					  var result = JsonSerializer.Deserialize<List<Sklad>>(resultRequest);
+					  Sklads = new ObservableCollection<Sklad>(result);
+					  OnPropertyChanged("Sklads");
+				  }));
+			}
+		}
+
+		private ButtonCommand toyGetCommand;
+		public ButtonCommand ToyGetCommand
+		{
+			get
+			{
+				return toyGetCommand ??
+				  (toyGetCommand = new ButtonCommand(obj =>
+				  {
+					  var resultRequest = client.SendRequest("gettoys");
+					  var result = JsonSerializer.Deserialize<List<Toy>>(resultRequest);
+					  Toys = new ObservableCollection<Toy>(result);
+					  OnPropertyChanged("Toys");
+				  }));
+			}
+		}
+
+		private ButtonCommand journalGetCommand;
+		public ButtonCommand JournalGetCommand
+		{
+			get
+			{
+				return journalGetCommand ??
+				  (journalGetCommand = new ButtonCommand(obj =>
+				  {
+					  var resultRequest = client.SendRequest("getjournals");
+					  var result = JsonSerializer.Deserialize<List<Journal>>(resultRequest);
+					  Journals = new ObservableCollection<Journal>(result);
+					  OnPropertyChanged("Journals");
+				  }));
+			}
+		}
 
 		public event PropertyChangedEventHandler PropertyChanged;
 		public void OnPropertyChanged([CallerMemberName] string prop = "")
